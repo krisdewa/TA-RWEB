eb800e1fbd7a8cbd84228aba0840c0adb17c49a0
Muhammad Naufal Hafizh <56239834+NaufalHafizh@users.noreply.github.com>
2022-07-14 13:56:41 +0700

    Update detail_v.php

--- application/views/Utama/detail_v.php
+++ application/views/Utama/detail_v.php
@@ -1,6 +1,6 @@
 <section class="container">
     <h1>Detail Jurnal</h1>
-    <h3><?= $data_detail->judul ?></h3>
+    <h4><?= $data_detail->judul ?></h4>
     <p> <?= $data_detail->penulis ?>, <span><?= $data_detail->tahun ?></span> </p>
     <p class="lead"><?= $data_detail->abstrak ?></p>
 
