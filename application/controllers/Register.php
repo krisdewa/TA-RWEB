35919d7135276edc00353d4cbf442f5c0c6fce1f
KrisnaDewa <59174531+krisdewa@users.noreply.github.com>
2022-07-15 12:51:46 +0700

    Update Register.php

--- application/controllers/Register.php
+++ application/controllers/Register.php
@@ -10,5 +10,24 @@ class Login extends CI_Controller {
     public function index(){
         $this->load->view('Auth/register'); //load view login
     }
+    //method add digunakan untuk menampilkan form tambah data jurnal
+    function add_account()
+    {
+        $register = $this->register_model; //objek model
+        $validation = $this->form_validation; //objek form validation
+        $validation->set_rules($register->rules()); //menerapkan rules validasi pada jurnal_model
+        //kondisi jika semua kolom telah divalidasi, maka akan menjalankan method save pada jurnal_model
+        if ($validation->run()) {
+            $register->save();
+            $this->session->set_flashdata('message', '<div class="alert alert-success alert-dismissible fade show" role="alert">
+            Register berhasil, Silahkan login untuk melanjutkan. </div>');
+        
+            redirect("Login");
+        }
+
+        $data["title"] = "Register account"; //judul halaman
+        $this->load->view('adminjurnal/add', $data);
+
+    }
 }
 
