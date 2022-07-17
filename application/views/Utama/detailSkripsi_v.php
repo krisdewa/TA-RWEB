77b3a1a7ee078c2d05bc663aa9beec21e2d4c5fa
Muhammad Naufal Hafizh <56239834+NaufalHafizh@users.noreply.github.com>
2022-07-17 16:34:40 +0700

    perbaiki tapilan detail

--- application/views/Utama/detailSkripsi_v.php
+++ application/views/Utama/detailSkripsi_v.php
@@ -1,8 +1,38 @@
-<section class="container">
-    <h1>Detail Skripsi</h1>
+<section style="margin-top: 50px; margin-bottom: 50px;" class="container">
+    <h1 class="mb-3">Detail Skripsi</h1>
+    <hr>
     <h4><?= $detail_skripsi->Judul ?></h4>
-    <p> <?= $detail_skripsi->penulis ?>, <span><?= $detail_skripsi->tahun ?></span> </p>
+    <p class="mt-5 fw-bold">Abstrak</p>
     <p class="lead"><?= $detail_skripsi->abstrak ?></p>
+    <hr>
+    <h5 class="mt-4">Informasi Tambahan</h5>
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">Properti</th>
+                <th scope="col">Nilai Properti</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th>penulis</th>
+                <td><?= $detail_skripsi->penulis ?></td>
+            </tr>
+            <tr>
+                <th>Penerbit</th>
+                <td><?= $detail_skripsi->penerbit ?></td>
+            </tr>
+            <tr>
+                <th>Tahun</th>
+                <td><?= $detail_skripsi->tahun ?></td>
+            </tr>
+            <tr>
+                <th>Jenis Penelitian</th>
+                <td><?= $detail_skripsi->jenis_penelitian ?></td>
+            </tr>
+        </tbody>
+    </table>
+    <hr>
     <?php
     $session_login = $this->session->userdata('nama');
     if (isset($session_login)) :
