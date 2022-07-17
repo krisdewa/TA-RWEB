555416a27b884714d637a32f08e5c3461bd0b559
KrisnaDewa <59174531+krisdewa@users.noreply.github.com>
2022-07-17 11:38:17 +0700

    fix

--- application/controllers/Beranda.php
+++ application/controllers/Beranda.php
@@ -4,7 +4,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Beranda extends CI_Controller {
 	public function index()
 	{
-		$this->load->view('Utama/templates/header');
+        $data = ['title' => "Beranda"];
+		$this->load->view('Utama/templates/header', $data);
         $this->load->view('Utama/templates/navbar');
         $this->load->view('Utama/beranda_v');
         $this->load->view('Utama/templates/footer');
